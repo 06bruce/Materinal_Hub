4db396fd504aedfd39b5099a3d67b54a3ec01862
06bruce <brucecnshuti2@gmail.com>
2025-10-07 12:28:54 +0200

    refactor: restructure repository - move frontend to root and remove backend
    - Move all frontend files from frontend/ to root directory
    - Remove backend folder (moved to separate Maternal_Server repo)
    - Remove documentation files (CHATBOT_DEMO, DEPLOYMENT, DESIGN_SYSTEM, etc.)
    - Remove docker-compose.yml, mongo-init.js, start-dev.sh
    - Repository now contains only frontend code at root level

--- backend/routes/chat.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { sendMessage } = require('../controllers/chatController');
-
-const router = express.Router();
-
-// Chat endpoint
-router.post('/', sendMessage);
-
-module.exports = router;
